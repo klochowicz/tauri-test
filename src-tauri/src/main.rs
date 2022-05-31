c94dcef965abd663025d7a6d329088dcd77ca29e
Mariusz Klochowicz <mariusz@klochowicz.com>
2022-05-31 22:34:53 +0930

    Add custom global menu items.
    Mostly boilerplate, this is not anything more sensible.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,32 @@
   windows_subsystem = "windows"
 )]
 
+use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
 fn main() {
+// here `"quit".to_string()` defines the menu item id, and the second parameter is the menu item label.
+  let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+  let close = CustomMenuItem::new("close".to_string(), "Close");
+  let submenu = Submenu::new("File", Menu::new().add_item(quit).add_item(close));
+  let menu = Menu::new()
+      .add_native_item(MenuItem::Copy)
+      .add_item(CustomMenuItem::new("hide", "Hide"))
+      .add_submenu(submenu);
+
   tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+      .manage(Database {})
+      .menu(menu)
+      .on_menu_event(|event| {
+        match event.menu_item_id() {
+          "quit" => {
+            std::process::exit(0);
+          }
+          "close" => {
+            event.window().close().unwrap();
+          }
+          _ => {}
+        }
+      })
+      .invoke_handler(tauri::generate_handler![my_custom_command])
+      .run(tauri::generate_context!())
+      .expect("error while running tauri application");
 }
